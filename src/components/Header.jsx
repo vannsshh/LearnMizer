89dd4a315b59843e9387617021fb6993bd8bc7f8
vannsshh <vanshoza472005@gmailcom>
2025-02-25 10:55:36 +0530

    Donee 2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="container-3 w-container">
-                <a href="#" className="link-block w-inline-block">
+                <a href="/" className="link-block w-inline-block">
                     <img
                         src="https://cdn.prod.website-files.com/6383abdde6636137533253d4/656c5ff5b3d2dc2abd108c63_Asset%201%20(6).png"
                         loading="lazy"
@@ -17,6 +17,7 @@ const Header = () => {
                     <a
                         href="https://app.Learnmizer.com"
                         target="_blank"
+                        rel="noopener noreferrer"
                         className="header-cta franchise-btn w-button"
                     >
                         Login
